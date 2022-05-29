15af435243621b16e2e17eb47447980812a7c304
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-05-29 21:35:06 +0200

    Refactor

--- src/main/java/eu/ciechanowiec/throwable/multicatch/Multicatch.java
+++ src/main/java/eu/ciechanowiec/throwable/multicatch/Multicatch.java
@@ -3,7 +3,6 @@ package eu.ciechanowiec.throwable.multicatch;
 import eu.ciechanowiec.throwable.Printer;
 import org.tinylog.Logger;
 
-import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
 /**
